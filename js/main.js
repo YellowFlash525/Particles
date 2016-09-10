b93fe177893551f09adecf732f9cd41a532f13d4
YellowFlash525 <magicproject525@gmail.com>
2016-09-10 16:49:05 +0200

    Create function draw and loop

--- js/main.js
+++ js/main.js
@@ -1,13 +1,38 @@
 var canvas = document.querySelector("#playground");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
 
 var ctx = canvas.getContext('2d');
-ctx.beginPath();
 ctx.fillStyle = "red";
 
-var loop = function() {
-	ctx.arc(Math.random() * 100, Math.random() * 100, 50, 0, Math.PI *2);
-	ctx.fill();
-	window.requestAnimationFrame(loop);
+var particles = [];
+
+var create = function() {
+	var dot = {
+		x: Math.random() * 250,
+		y: Math.random() * 150,
+		xVel: 10,
+		yVel: 10,
+		radius: 5
+	};
+
+	particles.push(dot);
+};
+
+var draw = function() {
+	ctx.clearRect(0, 0, canvas.width, canvas.heigh);
+	particles.forEach(function (dot) {
+		ctx.beginPath();
+		ctx.arc(dot.x, dot.y, dot.radius , 0, Math.PI *2);
+		ctx.fill();
+	});
 }
+
+var loop = function() {
+	create();
+	draw();
+	window.requestAnimationFrame(loop);
+};
 
 loop();
